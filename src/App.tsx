0e00c0703fdd64d2cb6d9798b03ce4a7419677ae
VictorLCosta <victorlc2019@outlook.com>
2022-11-20 15:04:27 -0300

    Project base

---
+++ src/App.tsx
@@ -0,0 +1,83 @@
+import { ChangeEvent, MutableRefObject, useRef, useState } from 'react'
+import issues from './data/issues.json'
+import './App.css'
+
+function App() {
+	const [checkedIssues, setCheckedIssues] = useState(new Set<string>())
+
+	const topCheckboxRef = useRef() as MutableRefObject<HTMLInputElement>;
+
+	const handleOnChange = (id: string) => {
+		let tempIssuesList = new Set(checkedIssues);
+
+		if (tempIssuesList.has(id)) {
+			tempIssuesList.delete(id)
+		} else {
+			tempIssuesList.add(id)
+		}
+
+		setCheckedIssues(tempIssuesList);
+
+		topCheckboxRef.current.indeterminate =
+			tempIssuesList.size > 0 && tempIssuesList.size < issues.length;
+
+		topCheckboxRef.current.checked = tempIssuesList.size === issues.length;
+	}
+
+	const handleSelectDeselectAll = (event: ChangeEvent<HTMLInputElement>) => {
+		if (event.target.checked) {
+			setCheckedIssues(new Set<string>(issues.map(({id}) => id)))
+		} else {
+			setCheckedIssues(new Set<string>())
+		}
+	}
+
+	return (
+		<table>
+			<thead>
+				<tr>
+					<th>
+						<input 
+							type="checkbox"
+							ref={topCheckboxRef}
+							onChange={e => handleSelectDeselectAll(e)}
+						/>
+					</th>
+					<th>
+						{checkedIssues.size === 0 ? (
+							'None selected'
+						) : (
+							`Selected ${checkedIssues.size}`
+						)}
+					</th>
+				</tr>
+				<tr>
+					<th></th>
+					<th>Name</th>
+					<th>Message</th>
+					<th>Status</th>
+				</tr>
+			</thead>
+			<tbody>
+				{issues.map(({id, name, message, status}, i) => {
+					const isIssueOpen = status === "open";
+
+					return (
+						<tr key={i} onClick={() => handleOnChange(id)} style={{ backgroundColor: checkedIssues.has(id) ? "#eee" : "#fff" }}>
+							<td>
+								<input type="checkbox" checked={checkedIssues.has(id)} onChange={() => handleOnChange(id)}/>
+							</td>
+							<td>{name}</td>
+							<td>{message}</td>
+							<td>
+								<span className={isIssueOpen ? 'red-circle' : 'green-circle'} />
+							</td>
+						</tr>
+					)
+				})}
+			</tbody>
+		</table>
+	)
+}
+
+export default App
